2d777366706f8d64b9dc56950c27e69ed4bcc797
Wei Kuang <50929180+kuangwei8@users.noreply.github.com>
2019-10-18 08:29:18 -0700

    still need more work

---
+++ PigDice/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace PigDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Player 1
+            int Player1OverallScore = 0;
+            
+            //Player2
+            int Plyaer2OverallScore = 0;
+
+           
+
+            RollDice player1 = new RollDice();
+            RollDice player2 = new RollDice();
+
+            string currentplayer = "player1";
+            Console.WriteLine("\n Enter h to hold your points or any other key to roll again.");
+            player1.Play();
+            //if(player1. GetCurrentScore() == 0 )
+            //{
+            //    player2.Play();
+            //}
+            //else
+            //{
+                
+            //}
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+
+
+  
+
+       
+
+    
+}
---
+++ PigDice/RollDice.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PigDice
+{
+    public class RollDice
+    {
+        int CurrentScore = 0;
+        int OverallScore = 0;
+
+        public int GetCurrentScore()
+        {
+            return CurrentScore;
+        }
+
+        public int GetOverallScore()
+        {
+            return OverallScore;
+        }
+        public RollDice()
+        {
+
+        }
+        public int generate() // generating 2 random numbers
+        {
+            Random dice = new Random();
+            int Dice1 = dice.Next(1, 7);
+            int Dice2 = dice.Next(1, 7);
+            int Score = 0;
+                if (Dice1 != 1 && Dice2 != 1)
+                {
+                    Score += (Dice1 + Dice2);
+                }
+                else { Score = 0; }
+            
+            Console.WriteLine($"Your dice numbers are {Dice1} and {Dice2}");
+          
+            return Score; 
+        }
+
+        public ConsoleKey Play()
+        {
+
+            ConsoleKey type = ConsoleKey.A;
+            while (OverallScore < 99)
+            {
+               
+                type = Console.ReadKey().Key;
+                Console.Clear();
+
+                int tempScore = generate();
+
+                if (tempScore != 0)
+                {
+                    CurrentScore += tempScore; 
+                }
+                else CurrentScore = 0; 
+
+                if (type == ConsoleKey.H)
+                {
+                    OverallScore += CurrentScore;
+                    CurrentScore = 0; 
+                }
+
+                Console.WriteLine($"Current Score is {CurrentScore}");
+                Console.WriteLine($"Overall Score is {OverallScore}");
+            }
+            
+
+            if (OverallScore > 99)
+            {
+                Console.WriteLine("You Won!");  
+            }
+
+            return type;
+
+        }
+    }
+
+}
